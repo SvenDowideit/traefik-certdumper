1ce58f0d445279dd9e34b6bbba106bf1bee16ede
Sven Dowideit <SvenDowideit@home.org.au>
2018-11-13 22:49:07 +1000

    fixes
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- run.sh
+++ run.sh
@@ -5,18 +5,18 @@ function dump() {
     ln -f /traefik/ssl/certs/* /traefik/ssl/
     ln -f /traefik/ssl/private/* /traefik/ssl/
     for crt_file in $(ls /traefik/ssl/certs/*); do
-        pem_file=$(echo $crt_file | sed 's/.crt/-public.pem/g' | sed 's/certs/pem/g')
+        pem_file=$(echo $crt_file | sed 's/certs/pem/g' | sed 's/.crt/-public.pem/g')
         echo "openssl x509 -inform PEM -in $crt_file > $pem_file"
         openssl x509 -inform PEM -in $crt_file > $pem_file
     done 
     for key_file in $(ls /traefik/ssl/private/*); do
-        pem_file=$(echo $key_file | sed 's/.key/-private.pem/g' | sed 's/private/pem/g')
+        pem_file=$(echo $key_file | sed 's/private/pem/g' | sed 's/.key/-private.pem/g')
         echo "openssl rsa -in $key_file -text > $pem_file"
         openssl rsa -in $key_file -text > $pem_file
     done
 }
 
-mkdir -p /traefik/pem/
+mkdir -p /traefik/ssl/pem/
 # run once on start to make sure we have any old certs
 dump
 
