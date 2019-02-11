bf2a05294847589f9ec4bb8945ab759e6265cf67
Thomas Vaillant <thomas.vaillant@formetris.com>
2019-02-11 16:12:39 +0100

    fix: improve logging

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 
+###
+# log()
+#
+function log() {
+    echo "[$(date)] $@"
+}
+
+log "Starting dumper into $CERTDUMPER_MODE mode"
+
 if [ $CERTDUMPER_MODE == "consul" ]; then
     exec /run_consul.sh
 else
